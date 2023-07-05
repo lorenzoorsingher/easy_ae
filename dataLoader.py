9712dc7ad7ca3688e0c09092b9c78fc48e2f457d
lollozip <lorenzo.orsingher@gmail.com>
2023-07-05 16:04:03 +0200

    add normalization to dataset

--- dataLoader.py
+++ dataLoader.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 import os
+import json 
+import cv2 as cv
 
 class CustomDataset(Dataset):
     def __init__(self):
@@ -20,24 +22,74 @@ class CustomDataset(Dataset):
             ypath = self.bwpath + "bw_"+str(i)+".jpg"
             if os.path.exists(Xpath) and os.path.exists(ypath):
                 self.data.append([Xpath,ypath])
-                
+        
+        # Opening JSON file
+        f = open('data.json')
+        self.jsondata = json.load(f)
+        f.close()
+
         self.img_dim = (80, 80)
         self.out_size = (79,79)
+
+    def normalize(self, Ximg, yimg):
+
+        yimg = yimg.astype(np.float64)
+        
+        yimg[:,:,0] -= self.jsondata["mean"]["vmc0"]
+        yimg[:,:,1] -= self.jsondata["mean"]["vmc1"]
+        yimg[:,:,2] -= self.jsondata["mean"]["vmc2"]
+        
+        yimg[:,:,0] = yimg[:,:,0] / self.jsondata["std"]["stdc0"]
+        yimg[:,:,1] = yimg[:,:,1] / self.jsondata["std"]["stdc1"]
+        yimg[:,:,2] = yimg[:,:,2] / self.jsondata["std"]["stdc2"]
+
+
+        
+        Ximg = Ximg.astype(np.float64)
+
+        Ximg -= self.jsondata["mean"]["vmbw"]
+
+        Ximg = Ximg / self.jsondata["std"]["stdbw"]
+
+        return Ximg, yimg
+
+    def denormalize(self, Ximg, yimg):
+
+        if not Ximg is None:
+            Ximg = Ximg * self.jsondata["std"]["stdbw"]
+            Ximg += self.jsondata["mean"]["vmbw"]
+        
+        if not yimg is None:
+            yimg[:,:,0] = yimg[:,:,0] * self.jsondata["std"]["stdc0"]
+            yimg[:,:,1] = yimg[:,:,1] * self.jsondata["std"]["stdc1"]
+            yimg[:,:,2] = yimg[:,:,2] * self.jsondata["std"]["stdc2"]
+
+            yimg[:,:,0] += self.jsondata["mean"]["vmc0"]
+            yimg[:,:,1] += self.jsondata["mean"]["vmc1"]
+            yimg[:,:,2] += self.jsondata["mean"]["vmc2"]
+
+        return Ximg, yimg
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
         color_path, bw_path = self.data[idx]
-        Ximg = cv2.imread(bw_path)
-        Ximg = cv2.cvtColor(Ximg, cv2.COLOR_BGR2GRAY)
-        Ximg = cv2.resize(Ximg, self.img_dim)
-        #Ximg = Ximg/255
-        Ximg_tensor = torch.tensor([Ximg])
 
+        Ximg = cv2.imread(bw_path)
         yimg = cv2.imread(color_path)
+
+        Ximg = cv2.cvtColor(Ximg, cv2.COLOR_BGR2GRAY)
+
+        Ximg = cv2.resize(Ximg, self.img_dim)
         yimg = cv2.resize(yimg, self.out_size)
-        #yimg = yimg/255
+
+        Ximg, yimg = self.normalize(Ximg, yimg)
+
+        Ximg_tensor = torch.tensor([Ximg])
         yimg_tensor = torch.tensor(yimg)
+
+        Ximg_tensor = Ximg_tensor.permute(0, 2, 1)
         yimg_tensor = yimg_tensor.permute(2, 1, 0)
+
         return Ximg_tensor, yimg_tensor
